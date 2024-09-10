2ed38ecbbba35fd1773879950c6504a1cf27ce68
vschool4566 <serviceharbour2024@gmail.com>
2024-09-10 15:36:34 -0400

    ss
    ss

--- routers/messageRouter.js
+++
@@ -1,8 +0,0 @@
-const {messagePost,getMessages}=require('../controllers/messageController')
-const router=require('express').Router()
-const { authenticateToken } = require('../middleware/authToken')
-
-router.route('/messagepost').post(authenticateToken,messagePost)
-router.route('/getmessages/:id').get(authenticateToken,getMessages)
-
-module.exports=router
